c409ba2dc47cc85d69c55f5dbd2468414f57213c
smok <smoksmk@gmail.com>
2019-10-14 21:59:48 +0300

    some code

--- __init__.py
+++ __init__.py
@@ -2,16 +2,30 @@ from .fields import Field
 
 
 class Schema:
-    def __init__(self, pre_load=None, post_load=None):
-        pass
+    def __init__(self, pre_load=None, post_load=None, many=False):
+        self._many = many
 
-    def load(self, data):
+    def _prepare_data(self, data):
         for key, value in data.items():
             attr = getattr(self, key, None)
             if hasattr(self, key):
                 if isinstance(attr, Field):
+                    print('instance')
                     setattr(self, key, attr(value))
                 elif isinstance(attr, Schema):
+                    print('Schema', key, attr)
                     setattr(self, key, attr.load(value))
+
+    def load(self, data):
+
+        if self._many and isinstance(data, list):
+            res = []
+            for dt in data:
+                res.append(self._prepare_data(dt))
+
+            return res
+
+        else:
+            self._prepare_data(data)
 
         return self
--- fields.py
+++ fields.py
@@ -1,21 +1,39 @@
-class Field:
+from abc import ABC
+
+
+class Field(ABC):
+    field_type = None
+
+    def __init__(self, strict=False):
+        self._strict = strict
+
     def __call__(self, value):
-        return value
+        try:
+            return self.field_type(value)
+        except TypeError:
+            raise TypeError if self._strict else None
 
     def __repr__(self):
         return ','.join(self.__dict__)
 
 
 class String(Field):
-    pass
+    field_type = str
 
 
 class Integer(Field):
-    pass
+    field_type = int
+
+
+class List(Field):
+    field_type = list
 
 
 class Nested(Field):
-    def __init__(self, field):
+    field_type = dict
+
+    def __init__(self, field, strict=False):
+        super().__init__(strict)
         self.field = field
 
     def __call__(self, *args, **kwargs):
