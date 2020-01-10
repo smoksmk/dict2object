33e0c358fb460d26f4978ab65d0f6ac832d0ee9d
igor.chepko <igor.chepko@south.rt.ru>
2020-01-10 16:32:45 +0300

    add default values

--- __init__.py
+++ __init__.py
@@ -16,12 +16,22 @@ def proxy_factory(data_object_class):
 class DataObject:
     _fields: dict
 
-    def __init__(self, data: dict):
+    def __init__(self, data: dict = None, extend=False):
         self._data = data
-        for name, value in data.items():
+        if not data:
+            data = {key: getattr(self, key) for key in self._fields.keys() if hasattr(self, key)}
 
+        self.__load(data, extend)
+
+    def __load(self, data, extend):
+        for name, value in data.items():
             if name in self._fields:
-                value = self._fields[name](value)
+                if inspect.isclass(self._fields.get(name)) and issubclass(self._fields.get(name), DataObject):
+                    value = self._fields[name](value, extend=extend)
+                else:
+                    value = self._fields[name](value)
+                setattr(self, name, value)
+            elif extend:
                 setattr(self, name, value)
 
     @classmethod
@@ -34,6 +44,8 @@ class DataObject:
                 fields[name] = FACTORIES[DataObject](hint)
             elif issubclass(type(hint), T._GenericAlias):
                 fields[name] = FACTORIES[T.get_origin(hint)](T.get_args(hint)[0])
+            elif hint in (str, int, bool):
+                fields[name] = hint
             else:
                 pass
 
@@ -51,7 +63,7 @@ class DataObject:
     def get_dict(self):
         fields = {}
         for name, value in self.__dict__.items():
-            if not name.startswith('_') and not name.startswith('get'):
+            if not name.startswith('_') and not name.startswith('get_'):
                 fields.update(self.__dictify(name, value))
         return fields
 
